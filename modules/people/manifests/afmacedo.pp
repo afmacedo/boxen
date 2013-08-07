e1507029375ae3f3e9022f814a78c234909c8916
Andre F. Macedo <afm.toys@gmail.com>
2013-08-07 14:59:27 -0300

    Add support to dotfiles

--- modules/people/manifests/afmacedo.pp
+++ modules/people/manifests/afmacedo.pp
@@ -12,4 +12,38 @@ class people::afmacedo {
   include keepassx
   include chrome
   #include vagrant
+
+  $home = "/Users/${::boxen_user}"
+  $dotfiles = "${home}/dotfiles"
+  $bundle = "${dotfiles}/vim/bundle"
+  $nerdtree = "${bundle}/nerdtree"
+
+  file { $dotfiles:
+    ensure => "directory",
+    owner => $::luser,
+    group => "staff"
+  }
+
+  repository { $dotfiles:
+    source => "afmacedo/dotfiles",
+    require => File[$dotfiles]
+  }
+
+  file { [ $bundle, $nerdtree ]:
+    ensure => "directory",
+    owner => $::luser,
+    group => "staff"
+  }
+
+  repository { $nerdtree:
+    source => "scrooloose/nerdtree.git",
+    require => File[$nerdtree]
+  }
+ 
+  exec { "make install":
+    cwd => "${dotfiles}",
+    command => "/usr/bin/make",
+    creates => "${home}/.bashrc",
+    require => Repository[$dotfiles]
+  }
 }
